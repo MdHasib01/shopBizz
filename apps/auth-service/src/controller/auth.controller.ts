78ede22642cfb0a88e922c25aef7e6d73f563b53
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-05-30 13:50:14 +0600

    complete the login with token and cookie

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import {
   checkOtpRestrictions,
   sendOtp,
@@ -9,6 +10,7 @@ import {
 } from "../utils/auth.helper";
 import { BadRequestError } from "../../../../packages/errorHandler";
 import prisma from "../../../../packages/libs/prisma";
+import { setCookies } from "../utils/cookies/setCookies";
 
 //Register a new user
 export const userRegistration = async (
@@ -69,5 +71,72 @@ export const verifyUser = async (
     });
   } catch (error) {
     return next(error);
+  }
+};
+
+// Login user
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new BadRequestError("Missing required fields");
+    }
+
+    const user = await prisma.users.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new BadRequestError("User not found");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password!);
+
+    if (!isMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // Generate JWT token
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.ACCESS_TOKEN_SECRET as string,
+      {
+        expiresIn: "15m",
+      }
+    );
+    const refreshToken = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.REFRESH_TOKEN_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    // store refresh token on http cecret cookie
+    setCookies(res, "refreshToken", refreshToken);
+    setCookies(res, "accessToken", refreshToken);
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      data: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    return next(err);
   }
 };
--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,9 +1,14 @@
 import express, { Router } from "express";
-import { userRegistration, verifyUser } from "../controller/auth.controller.js";
+import {
+  login,
+  userRegistration,
+  verifyUser,
+} from "../controller/auth.controller.js";
 
 const router: Router = express.Router();
 
 router.post("/register", userRegistration);
 router.post("/verify-user", verifyUser);
+router.post("/login", login);
 
 export default router;

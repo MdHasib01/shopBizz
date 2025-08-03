dd08d1d378d12202cd1511e5471e221c98f8b85b
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-08-04 00:47:57 +0600

    fixing axios instance for refresh tokne issue

--- apps/seller-ui/src/utils/axiosInstance.ts
+++ apps/seller-ui/src/utils/axiosInstance.ts
@@ -1,30 +1,6 @@
 import { apiConfig } from "@/configs/apiRoutes";
 import axios from "axios";
 
-const getCookie = (name: string): string | null => {
-  try {
-    const cookies = document.cookie.split(";");
-    console.log(cookies);
-
-    for (let cookie of cookies) {
-      const [cookieName, ...cookieValueParts] = cookie.trim().split("=");
-
-      if (cookieName === name) {
-        const cookieValue = cookieValueParts.join("="); // Handle values with '=' in them
-        console.log(decodeURIComponent(cookieValue));
-        return cookieValue ? decodeURIComponent(cookieValue) : null;
-      }
-    }
-
-    return null;
-  } catch (error) {
-    console.error("Error parsing cookie:", error);
-    return null;
-  }
-};
-const getRefreshToken = (): string | null => {
-  return getCookie("refreshToken") || getCookie("seller-refresh-token");
-};
 const axiosInstance = axios.create({
   baseURL: apiConfig.baseUrl,
   withCredentials: true,
@@ -34,7 +10,7 @@ let isRefreshing = false;
 let refreshSubscribers: (() => void)[] = [];
 
 export const handleLogout = () => {
-  // if (window.location.pathname !== "/login") window.location.href = "/login";
+  if (window.location.pathname !== "/login") window.location.href = "/login";
 };
 
 const subscriberTokenRefresh = (callback: () => void) => {
@@ -66,12 +42,10 @@ axiosInstance.interceptors.response.use(
       originalRequest._retry = true;
       isRefreshing = true;
       try {
-        const refreshToken = getRefreshToken();
         await axios.post(
           `${apiConfig.baseUrl}${apiConfig.routes.refreshToken}`,
           {},
           {
-            headers: { Authorization: `Bearer ${refreshToken}` },
             withCredentials: true,
           }
         );
@@ -82,7 +56,7 @@ axiosInstance.interceptors.response.use(
         return axiosInstance(originalRequest);
       } catch (error) {
         isRefreshing = false;
-        handleLogout();
+        // handleLogout();
         refreshSubscribers = [];
         return Promise.reject(error);
       }

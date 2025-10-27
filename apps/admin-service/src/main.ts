3e30f1459fc0665c1d743e6bef926becb2a3689a
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-10-27 10:52:18 +0600

    setup basic router for admin service

--- apps/admin-service/src/main.ts
+++ apps/admin-service/src/main.ts
@@ -1,17 +1,18 @@
-import express from "express";
+import express, { Router } from "express";
 import cookieParser from "cookie-parser";
-import { error } from "console";
 import { errorMiddleware } from "@packages/errorHandler/errorMiddleware";
+import router from "./routes/admin.route";
 
 const app = express();
 app.use(express.json({ limit: "100mb" }));
 app.use(cookieParser());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send({ message: "Welcome to admin-service!" });
 });
 
 // routes
+app.use("/api", router);
 
 app.use(errorMiddleware);
 const port = process.env.PORT || 6005;

200687eb40b0d59285e5305151fe2e77af32fad2
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-11-02 00:35:01 +0600

    create recommendation system

---
+++ apps/recommendation-service/src/main.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to recommendation-service!" });
+});
+
+// Routes
+
+const port = process.env.PORT || 6007;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on("error", console.error);
